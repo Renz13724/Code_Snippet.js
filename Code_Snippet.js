21c3314e6d6d8ebf264fd726f4c64f20939dc2f7
VALDEZ R. <renzv753@gmail.com>
2024-10-05 23:43:22 +0800

    Update Code_Snippet.js

--- Code_Snippet.js
+++ Code_Snippet.js
@@ -1,12 +1,12 @@
-uname = input("Renz Valdez");
-pword = input("RenzValdez12345");
-repword = input("RenzValdez12345");
+uname = input("Username");
+pword = input("Password");
+repword = input("Re-enter Password");
 if repword == pword:
   # compare the Registered Password and Re-entered Password
   # window Pop-up to Congratulate the user for registering.
-  uname1 = input("RenzValdez:");
+  uname1 = input("Username:");
   # declare a variable that will hold the uname on Login
-  password = input("RenzValdez12345:");
+  password = input("Password:");
   # compare the registered uname and entered uname on Login
   if uname1 == uname and password == pword:
     # compare the registered pword and entered pword on Login
